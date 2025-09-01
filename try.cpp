588bc1ef42efc2c238f3423fd1e32ab0eb2d93c7
Carinsay <3624089541@qq.com>
2025-09-01 21:42:35 +0800

    提交1次

---
+++ try.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;   
+int main()           
+{               
+    int n,m;
+    cin>>n>>m;
+    if(floor(m*150%)>n)  
+    {
+        return 1;
+    }    
+    int k,s;
+    for(int i =0;i<n;i++)
+    {
+        cin>>k>>s;
+        if()
+    }
+
+}   
